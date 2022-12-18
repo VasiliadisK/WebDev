8a1ee7cc45aa68a4ed581f2546b14f2112d0e1b6
trollge <kostasvasiliadis123@gmail.com>
2022-12-18 20:01:27 +0200

    Make appointment start

--- assets/js/main.js
+++ assets/js/main.js
@@ -186,33 +186,7 @@
   });
 
   /**
-   * Testimonials slider
-   */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 20
-      },
-
-      1200: {
-        slidesPerView: 2,
-        spaceBetween: 20
-      }
-    }
-  });
+   
 
   /**
    * Initiate Pure Counter 
