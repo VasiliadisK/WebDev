b7d4c0bb344fdb2316881bf3a6bfbf23b7e28566
AJavaClass <kostasgianprod@gmail.com>
2022-12-29 14:18:51 +0100

    Added dbname variable in db.php.

--- db.php
+++ db.php
@@ -2,13 +2,12 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
+$dbname = "docwebox";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
-
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
 ?>
